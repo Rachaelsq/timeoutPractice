4319c3197a4162688b795b49c9f911473a79650d
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2020-12-09 23:23:21 -0600

    getting started- initial commit

---
+++ main.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+    console.log("Our timeout chain is starting...")
+}, 3000);
+console.log("get ready...")
+
+document.getElementById("buttonId")
+buttonId.addEventListener("click", function {
+    console.log("this is the function event")
+}
