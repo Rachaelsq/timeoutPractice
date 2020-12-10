891670700da75d177fc3cd7f31899ed64abf3973
Rachaelsq <intuitiveharmonymedicine@gmail.com>
2020-12-09 23:36:33 -0600

    got a timeout to work
    button with an event listener prints text after 5 seconds

--- main.js
+++ main.js
@@ -3,7 +3,17 @@ setTimeout(() => {
 }, 3000);
 console.log("get ready...")
 
-document.getElementById("buttonId")
-buttonId.addEventListener("click", function {
-    console.log("this is the function event")
-}
+// document.getElementById("buttonId")
+//  buttonId.addEventListener("click", function() {
+//      console.log("this is the function event")
+//  }
+
+document.getElementById("buttonId").addEventListener("click", function() {
+
+setTimeout(() => {
+    document.getElementById("p").innerHTML = "helloooooooo timeout";
+
+}, 5000);
+
+    // document.getElementById("p").innerHTML = "helloooooooo timeout";
+});
